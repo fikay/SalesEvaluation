d1b91ad78b9f65b8dceb5edf0f8e0ca370ce2b5c
fikayo <fikayofakeye@yahoo.com>
2025-08-17 12:08:46 -0600

    Add Nat gateway to subnets

--- Infra/modules/virtual_machine_module/subnets.tf
+++ Infra/modules/virtual_machine_module/subnets.tf
@@ -14,8 +14,21 @@ resource "azurerm_subnet" "sales_evaluation_private_subnet" {
 }
 
 resource "azurerm_subnet" "sales_evaluation_bastion_subnet" {
-  name                 = "sales_evaluation_public_subnet"
+  name                 = "sales_evaluation_bastion_public_subnet"
   resource_group_name  = data.azurerm_resource_group.existingResourceGroup.name
   virtual_network_name = azurerm_virtual_network.sales_evaluation_vnet.name
   address_prefixes = ["10.0.2.0/24"]
+}
+
+
+
+
+resource "azurerm_subnet_nat_gateway_association" "ng_private_subnet_association" {
+  subnet_id      = azurerm_subnet.sales_evaluation_private_subnet.id
+  nat_gateway_id = azurerm_nat_gateway.ngateway.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "ng_bastion_subnet_association" {
+  subnet_id      = azurerm_subnet.sales_evaluation_bastion_subnet.id
+  nat_gateway_id = azurerm_nat_gateway.ngateway.id
 }
